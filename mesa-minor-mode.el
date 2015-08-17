905fbb8d31a680d11e0379373fb213d0a19c2bae
Josiah Schwab <jschwab@gmail.com>
2015-08-16 20:39:48 -0700

    Major update and refactor

--- mesa-minor-mode.el
+++ mesa-minor-mode.el
@@ -1,32 +1,22 @@
 ;;; mesa-minor-mode.el --- tools for the MESA stellar evolution code
 
-;; Copyright (C) 2013-2014  Josiah Schwab
-
 ;; Author: Josiah Schwab <jschwab@gmail.com>
 ;; Keywords: files
 
-;; This software is under the MIT License
-;; http://opensource.org/licenses/MIT
+;; Copyright (C) 2013-2015 Josiah Schwab
 
-;; Permission is hereby granted, free of charge, to any person
-;; obtaining a copy of this software and associated documentation
-;; files (the "Software"), to deal in the Software without
-;; restriction, including without limitation the rights to use, copy,
-;; modify, merge, publish, distribute, sublicense, and/or sell copies
-;; of the Software, and to permit persons to whom the Software is
-;; furnished to do so, subject to the following conditions:
-
-;; The above copyright notice and this permission notice shall be
-;; included in all copies or substantial portions of the Software.
-
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-;; SOFTWARE.
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see http://www.gnu.org/licenses/.
 
 ;;; Commentary:
 
@@ -41,35 +31,137 @@
   :prefix "mesa-"
   :group 'mesa)
 
-(defcustom mesa-tags-file-name
+(defcustom mesa-tags-file-path
   "TAGS"
   "Name of the TAGS file inside of your MESA project"
   :group 'mesa)
 
-(defcustom mesa-tags-file-path
-  (concat (getenv "MESA_DIR") "/star/defaults/")
-  "Path to your TAGS file inside of your MESA project"
-  :group 'mesa)
-
 (defcustom mesa-tags-regexp
-  "'/[ \\t]+\\([^ \\t]+\\)[ \\t]*=/\\1/'"
+  "'/^[ \\t]+\\([^ \\t!]+\\)[ \\t]*=/\\1/'"
   "Regexp to recognize tags in defaults files"
+  :group 'mesa
+)
+
+(defcustom mesa-default-version
+  "git"
+  "Default version of MESA"
+  :group 'mesa
+  )
+
+(defcustom mesa-init-file
+  "~/.mesa_init"
+  "Default MESA init file"
   :group 'mesa
 )
 
 (defconst mesa-rse-include-line
   "      include 'standard_run_star_extras.inc'"
   "Line in run_star_extras.f that pulls in default code."
-)
+  )
+
+(defconst mesa-defaults-files
+  '("star/defaults/star_job.defaults"
+    "star/defaults/controls.defaults"
+    "star/defaults/pgstar.defaults"
+    "binary/defaults/binary_job.defaults"
+    "binary/defaults/binary_controls.defaults")
+  "Defaults files contained in MESA")
+
+
+;; The function mesa-read-init is based on ini.el
+;; License: GPL v2+
+;; Copyright: Daniel Ness
+;; URL: https://github.com/daniel-ness/ini.el
+
+(defun mesa-read-init (filename)
+  "Read a MESA config file"
+  (let ((lines (with-temp-buffer
+                 (insert-file-contents filename)
+                 (split-string (buffer-string) "\n")))
+        (section)
+        (section-list)
+        (alist))
+    (dolist (l lines)
+      (cond ((string-match "^;" l) nil)
+            ((string-match "^[ \t]$" l) nil)
+            ((string-match "^\\[\\(.*\\)\\]$" l)
+             (progn 
+               (if section
+                   ;; add as sub-list
+                   (setq alist (cons `(,section . ,section-list) alist))
+                 (setq alist section-list))
+               (setq section (match-string 1 l))
+               (setq section-list nil)))
+            ((string-match "^[ \t]*\\(.+\\) = \\(.+\\)$" l)
+             (let ((property (match-string 1 l))
+                   (value (match-string 2 l)))
+               (progn 
+                 (setq section-list (cons `(,property . ,value) section-list)))))))
+    (if section
+        ;; add as sub-list
+        (setq alist (cons `(,section . ,section-list) alist))
+      (setq alist section-list))
+    alist))
+
+(defun mesa-versions ()
+  "List the possible MESA versions"
+  (let ((mesa-init-data (mesa-read-init mesa-init-file)))
+    (mapcar 'car mesa-init-data)))
+
+(defun mesa-dir-from-version (version)
+  "Given a MESA version string, return the corresponding MESA_DIR"
+  (let ((mesa-init-data (mesa-read-init mesa-init-file)))
+    (cdr (assoc "MESA_DIR"
+                (cdr (assoc version mesa-init-data))))))
+
+(defun mesa~prepend-mesa-dir (filename)
+  "Append the MESA_DIR to a filename"
+  (let ((mesa-dir (mesa-dir-from-version mesa-version)))
+    (concat (file-name-as-directory mesa-dir) filename)))
+
+(defun mesa-tags-file ()
+  "Create the full path to the TAGS directory"
+  (mesa~prepend-mesa-dir mesa-tags-file-path))
+
+(defun mesa-visit-tags-table ()
+  "Visit tags table"
+  (let ((tags-add-tables nil))
+    (visit-tags-table (mesa-tags-file) t)))
+
+(defun mesa-change-tags-table ()
+  "Change tags table"
+  ;; this works, but I don't understand why it is necesary.  if I just
+  ;; used visit-tags-table, it would still always visit the old table,
+  ;; even though tags-file-name would have the right value...
+  (setq-local tags-table-list nil)
+  (add-to-list 'tags-table-list (mesa-tags-file))
+  (setq-local tags-file-name (mesa-tags-file))
+
+  ;; make TAGS file if it doesn't exist
+  (if (not (file-exists-p (mesa-tags-file)))
+      (mesa-regen-tags)))
+  
+(defun mesa-cleanup-tags-table ()
+  "Cleanup tags table"
+  (delete mesa-tags-file-name tags-table-list))
 
 (defun mesa-regen-tags ()
   "Regenerate the tags file for the MESA defaults directory"
   (interactive)
-  (shell-command (format "etags --language=none --regex=%s -o %s/%s %s*.defaults"
-                           mesa-tags-regexp
-                           mesa-tags-file-path
-                           mesa-tags-file-name
-                           mesa-tags-file-path)))
+  (shell-command (format "etags --language=none --regex=%s -o %s %s"
+                         mesa-tags-regexp
+                         (mesa-tags-file)
+                         (mapconcat 'mesa~prepend-mesa-dir mesa-defaults-files " "))))
+
+(defun mesa-change-version ()
+  "Change the MESA version being used in this buffer"
+  (interactive)
+  (setq-local mesa-version
+                (completing-read
+                 "Select MESA Version: "
+                 (mesa-versions)
+                 nil t))
+    (mesa-change-tags-table))
 
 (defun mesa-toggle-boolean ()
   "Toggle an inlist flag between .true. <--> .false."
@@ -100,45 +192,50 @@ file"
     (if (search-forward mesa-rse-include-line nil nil)
         (progn
           (narrow-to-region (line-beginning-position) (line-end-position))
-          (insert-file-contents 
-           (concat (getenv "MESA_DIR") "/include/standard_run_star_extras.inc")
+          (insert-file-contents
+           (mesa~prepend-mesa-dir "include/standard_run_star_extras.inc")
            nil nil nil t)
           (widen)))))
       
+(defcustom mesa-mode-line
+  '(:eval (format " MESA[%s]" mesa-version))
+  "Mode line lighter for MESA minor mode"
+  :type 'sexp
+  :risky t)
 
 (define-minor-mode mesa-minor-mode
   "Toggle MESA minor mode in the usual way."
   :init-value nil
   ;; The indicator for the mode line.
-  :lighter " MESA"
+  :lighter mesa-mode-line
   ;; The minor mode bindings.
   :keymap
   '(
     ("\C-c\C-t" . mesa-toggle-boolean)
     ("\C-c\C-c" . mesa-comment-dwim)
     ("\C-c\C-r" . mesa-enable-rse)
+    ("\C-c\C-v" . mesa-change-version)
     )
-  ;; the body
+  ;; The body
   (if mesa-minor-mode
 
       ;; turn mesa-minor-mode on
       (progn
 
-        (let ((mesa-tags-file (concat mesa-tags-file-path
-                                      mesa-tags-file-name)))
+        ;; set the MESA version
+        (setq-local mesa-version mesa-default-version)
 
-          ;; if TAGS file doesn't exist, generate it
-          (if (not (file-exists-p mesa-tags-file))
-              (mesa-regen-tags))
+        ;; make TAGS file if it doesn't exist
+        (if (not (file-exists-p (mesa-tags-file)))
+            (mesa-regen-tags))
 
-          ;; set the buffer-local tags file to the MESA file
-          (visit-tags-table mesa-tags-file)))
+        (mesa-visit-tags-table))
 
   ;; turn mesa-minor-mode off
     (progn
 
       ;; take MESA out of the global tags table list
-      (delete mesa-tags-file-name tags-table-list)))
+      (mesa-cleanup-tags-table)))
   ;; the group
   :group 'mesa
 )
